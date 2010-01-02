9a63b197b188e0890798b6d48bbafa58abbf1fa1
Sean Braithwaite <brapse@gmail.com>
2010-01-01 22:53:41 -0500

    Tag attributes

--- src/reindeer.js
+++ src/reindeer.js
@@ -6,14 +6,16 @@
 // By: Sean Braithwaite
 //
 // NOTE: won't work in IE
+// Try not to depend on jquery for now
 
 var Reindeer = function(){
     var supported_tags = ["span", "div", "a"];
 
-    var Stack = function(type){
+    var Stack = function(type, attrs){
 
         st = {};
         var _tag, _next, _text;
+        var _attrs = attrs || {};
 
         //XXX: js 1.6
         if(supported_tags.lastIndexOf(type) != -1){
@@ -33,7 +35,6 @@ var Reindeer = function(){
         };
 
         st.next = function(item){
-            console.log("nexting");
             if(item){
                 return (_next = item);
             }else{
@@ -54,17 +55,22 @@ var Reindeer = function(){
 
         st.render = function(){
             if(this.next()){
-                if(_tag){
-                    return "<" + _tag + ">" + this.next().render() + "</" + _tag + ">";
-                }else{
-                    return this.next().render();
-                }
+                return _tag ? this.render_tag(this.next().render()) : this.next().render();
             }else{
-                if(_text){
-                    return _text;
-                }else{
-                    return "<" + _tag + "/>";
-                }
+                return _text ? _text : this.render_tag();
+            }
+        };
+
+        st.render_tag = function(contents){
+            var tag_attributes = [_tag];
+            for(var prop in _attrs){
+                tag_attributes.push(prop + "=\"" + _attrs[prop] + "\"");
+            }
+
+            if(contents){
+                return "<" + tag_attributes.join(' ') + ">" + this.next().render() + "</" + _tag + ">";
+            }else{
+                return "<" + tag_attributes.join(' ') + "/>";
             }
         };
 
@@ -77,7 +83,6 @@ var Reindeer = function(){
     // ######################### 
    
     rd.render = function(){
-        console.log("rendering");
         return rd.stack.render();
     };
 
